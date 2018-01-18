16ba5c2c34d25562f33276107938afe4035e1e30
Nicolas Widart <n.widart@gmail.com>
2018-01-18 22:09:03 +0100

    🚀 First Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home.html.twig');
+    }
+}
