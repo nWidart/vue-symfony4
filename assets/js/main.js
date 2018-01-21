7394387037c826ecb1aab34b46cc479936f06e63
Nicolas Widart <n.widart@gmail.com>
2018-01-21 16:08:04 +0100

    Setup element with english locale

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 new Vue({
   el: '#app',
