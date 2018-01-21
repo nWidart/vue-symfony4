4911171fd0fb2bc0a76e858f6ec6c9390157a34e
Nicolas Widart <n.widart@gmail.com>
2018-01-21 16:45:07 +0100

    Setting up basic dashboard. @source: https://github.com/PanJiaChen/vueAdmin-template

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,39 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-class TestClassSyntax {
-
-}
-
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app'
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
-<style lang="scss">
-#app {
-  display: flex;
-  color: #2c3e90;
-}
-</style>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
-import App from './App'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import './styles/index.scss' // global css
+
+import App from './App'
+import router from './router'
+import store from './store'
 
 Vue.use(ElementUI, { locale });
 
 new Vue({
   el: '#app',
+  router,
+  store,
   template: '<App/>',
   components: { App }
 });
