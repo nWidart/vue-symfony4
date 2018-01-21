716784eda9f94ed9ddab4419fb7fda1f402ffedf
Nicolas Widart <n.widart@gmail.com>
2018-01-21 16:05:25 +0100

    Setup element-ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
