6f942a5af3371f5d2666fcb27257815b2d39e458
Nicolas Widart <n.widart@gmail.com>
2018-01-22 21:05:24 +0100

    Installing phpunit

---
+++ api/tests/FirstTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTestCase extends TestCase
+{
+    /** @test */
+    public function it_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
