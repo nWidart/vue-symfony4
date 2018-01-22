e6714e998a11d65c3324069517680a8b49741024
Nicolas Widart <n.widart@gmail.com>
2018-01-22 20:26:18 +0100

    Adding a real avatar url

--- ui/src/views/layout/components/Navbar.vue
+++ ui/src/views/layout/components/Navbar.vue
@@ -4,7 +4,7 @@
     <breadcrumb></breadcrumb>
     <el-dropdown class="avatar-container" trigger="click">
       <div class="avatar-wrapper">
-        <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+        <img class="user-avatar" :src="avatarUrl">
         <i class="el-icon-caret-bottom"></i>
       </div>
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
@@ -35,7 +35,10 @@ export default {
     ...mapGetters([
       'sidebar',
       'avatar'
-    ])
+    ]),
+    avatarUrl() {
+      return 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80';
+    }
   },
   methods: {
     toggleSideBar() {
