4a788c87ed00da9220e5ff01aa36576d27d5e2ff
Nicolas Widart <n.widart@gmail.com>
2018-01-22 21:19:57 +0100

    Testing the integration tests

--- api/tests/FirstTestCaseTest.php
+++ api/tests/FirstTestCaseTest.php
@@ -3,12 +3,51 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class FirstTestCaseTest extends TestCase
+class FirstTestCaseTest extends WebTestCase
 {
     /** @test */
     public function it_is_true()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * test getUsersAction
+     */
+    public function testGetUsers()
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', '/api/users');
+        // ...
+    }
+
+    /**
+     * Create a client with a default Authorization header.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @return \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected function createAuthenticatedClient($username = 'user', $password = 'password')
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/login_check',
+            array(
+                'username' => $username,
+                'password' => $password,
+            )
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+        return $client;
     }
 }
