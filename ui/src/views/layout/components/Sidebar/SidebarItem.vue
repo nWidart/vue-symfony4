ba3d0d9fc78b7c3dbaf244a671e51aaf27df7101
Nicolas Widart <n.widart@gmail.com>
2018-01-23 21:15:45 +0100

    Using font awesome icons. Adding dashboard item

--- ui/src/views/layout/components/Sidebar/SidebarItem.vue
+++ ui/src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="menu-wrapper">
     <template v-for="item in routes" v-if="!item.hidden&&item.children">
-
+      <router-link :to="{name: 'Dashboard'}">
+        <el-menu-item index="1-1">
+          <i class="fas fa-tachometer-alt"></i> <span>Dashboard</span>
+        </el-menu-item>
+      </router-link>
       <router-link v-if="item.children.length===1 && !item.children[0].children" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
         <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
-          <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>
+          <i v-if="item.children[0].meta&&item.children[0].meta.icon" :class="item.children[0].meta.icon"></i>
           <span v-if="item.children[0].meta&&item.children[0].meta.title">{{item.children[0].meta.title}}</span>
         </el-menu-item>
       </router-link>
 
       <el-submenu v-else :index="item.name||item.path" :key="item.name">
         <template slot="title">
-          <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
+          <i v-if="item.meta&&item.meta.icon" :class="item.meta.icon"></i>
           <span v-if="item.meta&&item.meta.title">{{item.meta.title}}</span>
         </template>
 
@@ -20,7 +24,7 @@
 
           <router-link v-else :to="item.path+'/'+child.path" :key="child.name">
             <el-menu-item :index="item.path+'/'+child.path">
-              <svg-icon v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
+              <i v-if="child.meta&&child.meta.icon" :class="child.meta.icon"></i>
               <span v-if="child.meta&&child.meta.title">{{child.meta.title}}</span>
             </el-menu-item>
           </router-link>
