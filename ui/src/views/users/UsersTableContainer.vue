b1c10a34e802f442ed90fd680b1f29c90b0d370a
Nicolas Widart <n.widart@gmail.com>
2018-01-23 21:20:41 +0100

    Preparing a user table container

---
+++ ui/src/views/users/UsersTableContainer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="users-container">
+        <h1>Users</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {},
+    mounted() {
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .users {
+        &-container {
+            margin: 30px;
+        }
+    }
+</style>
