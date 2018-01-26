33d404f5486101559f90229d3ae1787fcd904d9a
Nicolas Widart <n.widart@gmail.com>
2018-01-26 20:34:29 +0100

    Adding and using babel polyfill to use async/await

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'babel-polyfill'
 
 import './styles/index.scss' // global css
 import App from './App'
