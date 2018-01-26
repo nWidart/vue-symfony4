cac90ceed5e6fe70a48c2e1c72578d2ec6297eb6
Nicolas Widart <n.widart@gmail.com>
2018-01-26 21:09:54 +0100

    Display first go at a users table

---
+++ ui/src/views/users/UsersTable.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-table :data="users" v-loading.body="users.length === 0">
+            <el-table-column prop="attributes._id" label="id" sortable="custom"/>
+            <el-table-column prop="attributes.email" label="email" sortable="custom"/>
+            <el-table-column prop="attributes.username" label="username" sortable="custom"/>
+        </el-table>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      users: {
+        type: Array,
+        default: () => []
+      },
+    },
+    data() {
+      return {}
+    },
+    methods: {},
+    mounted() {
+    }
+  }
+</script>
--- ui/src/views/users/UsersTableContainer.vue
+++ ui/src/views/users/UsersTableContainer.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="users-container">
         <h1>Users</h1>
+        <users-table :users="users"/>
     </div>
 </template>
 
 <script>
+    import request from '../../utils/request'
+    import UsersTable from './UsersTable'
   export default {
+    components: {UsersTable},
+    component: [UsersTable],
     data() {
-      return {}
+      return {
+        users: [],
+        meta: [],
+        links: [],
+      }
     },
-    methods: {},
+    methods: {
+      async fetchUsers() {
+        const response = await request.get('/api/users');
+        console.log(response);
+        this.users = response.data.data;
+        this.meta = response.data.meta;
+        this.links = response.data.links;
+      }
+    },
     mounted() {
+      this.fetchUsers();
     }
   }
 </script>
